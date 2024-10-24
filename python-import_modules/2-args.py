a60c1144b79e992fdefd98bf793cf04d004d81d5
aziza20-ux <s.afadhali@alustudent.com>
2024-10-24 20:29:06 +0200

    maths modules

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
             print("{} argument:".format(length))
         else:
             print("{} arguments:".format(length))
-            i = 1
-        while i <= length:
-                print("{} : {}".format(i, argv[i]))
-                i += 1
+        i = 1
+        while(i <= length):
+            print("{}: {}".format(i, argv[i]))
+            i += 1
     else:
-        print("{} arguments".format(length))
+        print("{} arguments.".format(length))
 
