4b271402358e2388a55ba65a02b50ef071923252
aziza20-ux <s.afadhali@alustudent.com>
2024-10-24 21:33:45 +0200

    sum of arguments

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-   def adding_argument():
-       sum = ""
-       for i in sys.argv[1:]:
-           sum += int(i)
-           print(sum)
-adding_argument()
+    from sys import argv
+    length = len(argv) - 1
+    total_sum = 0
+    i = 1
+    if length == 0:
+        print("{}".format(total_sum))
+    else:
+        while i <= length:
+            total_sum += int(argv[i])
+            i += 1
+        print(total_sum)
