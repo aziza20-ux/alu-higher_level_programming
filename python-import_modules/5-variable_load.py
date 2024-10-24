cea89148c3f73fbf17a6b34ea3dac0802103c9a2
aziza20-ux <s.afadhali@alustudent.com>
2024-10-24 22:12:52 +0200

    sum of arguments

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__"
-from variable_load_5 import a
-a = 98
-print("{}".format(a))
+if __name__ == "__main__":
+    from variable_load_5 import a
+    print("{}".format(a))
