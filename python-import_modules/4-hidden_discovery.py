ad6950f97a20f7e77ca92d8406506dd47dfa6b95
aziza20-ux <s.afadhali@alustudent.com>
2024-10-24 21:58:27 +0200

    sum of arguments

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     for word in dir(hidden_4):
-        if not(word[0] == '_' and word[1] == '_'):
+        if not (word[0] == '_' and word[1] == '_'):
             print("{}".format(word))
 
