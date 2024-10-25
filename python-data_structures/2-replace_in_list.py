efe809140aa551807ea5f3a93c8de01fde7b77d9
aziza20-ux <s.afadhali@alustudent.com>
2024-10-25 12:23:44 +0200

    replacing elements

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -4,9 +4,5 @@ def replace_in_list(my_list, idx, element):
         return my_list
     if idx >= len(my_list):
         return my_list
-    i = 0
-    for ele in my_list:
-        if i == idx:
-            my_list[i] = element
-            i += 1
+    my_list[idx] = element
     return my_list
