c4c91da9fa2436491edb999f5b3d28fe877e6a89
aziza20-ux <s.afadhali@alustudent.com>
2024-10-25 14:13:01 +0200

    reversing

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    length == len(my_list)
-    if length == 0:
-        return None
+    if len(my_list) == 0:
+        return (None)
     large = my_list[0]
-    for i in range(length):
-        if my_list[i] > large:
-            my_list[i] = large
-    return large
+    for num in range(len(my_list)):
+        if my_list[num] > large:
+            large = my_list[num]
+
+    return (large)
