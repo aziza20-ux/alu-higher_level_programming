f17ce0191955425edeb8a85cd1da060453e13069
aziza20-ux <s.afadhali@alustudent.com>
2024-10-25 13:03:30 +0200

    reversing

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new = []
+    if idx < o:
+        return my_list
+    if idx >= len(my_list):
+        return my_list
+    new.extend(my_list)
+    new[idx] = element
+
