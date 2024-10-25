efebc4ceaafce804cc79c8650094e4c6aecaa11f
aziza20-ux <s.afadhali@alustudent.com>
2024-10-25 21:12:05 +0200

    square matrix

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -5,4 +5,4 @@ def print_sorted_dictionary(a_dictionary):
     keys = list(a_dictionary.keys())
     keys.sort()
     for i in keys:
-        print("{}:{}".format(i, a_dictionary[i]))
+        print("{}: {}".format(i, a_dictionary[i]))
