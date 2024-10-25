06446a04f5985c32888b0c72e5096d637b11dfda
aziza20-ux <s.afadhali@alustudent.com>
2024-10-25 14:20:13 +0200

    reversing

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    multiples = []
+    for num in range(len(my_list)):
+        if my_list[num] % 2 == 0:
+            multiples.append(True)
+        else:
+            multiples.append(False)
+    return (multiples)
