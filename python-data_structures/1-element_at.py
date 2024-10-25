7935eb38d7db08c2b33f6a61767ca21e29f3a3b0
aziza20-ux <s.afadhali@alustudent.com>
2024-10-25 12:02:23 +0200

    printing integers

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,12 +2,12 @@
 def element_at(my_list, idx):
     if idx == 0:
         return none
-    if idx <= leng(my_list):
+    if idx >= leng(my_list):
         return none
     i = 0
     for ele in my_list:
         if i == idx:
             break
         i += 1
-        return ele
+    return ele
         
