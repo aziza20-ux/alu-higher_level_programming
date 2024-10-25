2e92ce779f18b160adb30a1c676a99148e24ffaa
aziza20-ux <s.afadhali@alustudent.com>
2024-10-25 22:04:04 +0200

    square matrix

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# A Function that returns a key with the biggest integer value.
 
 
 def best_score(a_dictionary):
@@ -6,7 +7,7 @@ def best_score(a_dictionary):
         return None
     key = list(a_dictionary.keys())[0]
     largest = a_dictionary[key]
-    for k, v in list(a_dictionary.item()):
+    for k, v in a_dictionary.items():
         if v > largest:
             largest = v
             key = k
