4f097ba05a3d9295f8700ce064eeec1d25ae9629
aziza20-ux <s.afadhali@alustudent.com>
2024-10-25 20:23:51 +0200

    square matrix

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+# A Function that replaces all occurrences of an element by
+# another in a new list.
+
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
-    for i range(len(new_list)):
+
+    for i in range(len(new_list)):
         if new_list[i] == search:
             new_list[i] = replace
-    return (new_list)
+    return(new_list)
