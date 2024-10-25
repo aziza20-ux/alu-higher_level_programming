818d6d72268dd2458dd9cc0012b21fae4f6ac152
aziza20-ux <s.afadhali@alustudent.com>
2024-10-25 20:40:41 +0200

    square matrix

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+# A Function that adds all unique integers in a list
+# (only once for each integer).
+
 def uniq_add(my_list=[]):
     sum = 0
     for i in set(my_list):
-        sum += 1
+        sum += i
     return(sum)
