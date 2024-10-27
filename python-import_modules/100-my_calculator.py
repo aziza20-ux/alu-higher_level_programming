f7242f3cf65132ae11c65932f00eca0a31375d99
aziza20-ux <s.afadhali@alustudent.com>
2024-10-27 19:06:07 +0200

    modules

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -7,19 +7,19 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
         ops == argv[2]
-        if not (ops == "+" or ops == "-" or ops == "/" or ops == "*"):
-            print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-            exit(1)
-            a = int(argv[1])
-            b = int(argv[3])
-            if ops == +"":
-                print("{} + {} = {}".format(a, b, add(a, b)))
-            elif ops == "-":
-                print("{} - {} = {}".format(a, b, sub(a, b)))
-            elif ops == "/":
-                 print("{} / {} = {}".format(a, b, div(a, b)))
-            else:
-                 print("{} * {} = {}".format(a, b, mul(a, b)))
+    if not (ops == '+' or ops == '-' or ops == '/' or ops == '*'):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+        a = int(argv[1])
+        b = int(argv[3])
+    if (ops == '+'):
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif (ops == '-'):
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif (ops == '/'):
+        print("{} / {} = {}".format(a, b, div(a, b)))
+    else:
+        print("{} * {} = {}".format(a, b, mul(a, b)))
         
 
 
