26796415ce56d22dd22e757ebd1d51177ee86224
aziza20-ux <s.afadhali@alustudent.com>
2024-10-30 19:47:45 +0200

    printing only integers

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,10 +3,10 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i], end=""))
+            print("{:d}".format(my_list[i]), end="")
         except (TypeError, ValueError):
             pass
         else:
-        count += 1
+            count += 1
     print()
     return (count)
