8630fd08d1e83dc4500473e4589928c1df6ed3a2
aziza20-ux <s.afadhali@alustudent.com>
2024-10-31 14:04:23 +0200

    safe function

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -3,7 +3,7 @@ import sys
 def safe_function(fct, *args):
     try:
         ret = fct(*args)
-        return (ret)
+        return ret
     except exception as e:
         print("Exception:{}".format(e),file = sys.stderror)
-        return (None)
+        return None
