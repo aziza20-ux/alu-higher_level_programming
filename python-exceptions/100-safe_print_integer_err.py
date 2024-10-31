9915c281746ac74be8d295820c39c166bc77a758
aziza20-ux <s.afadhali@alustudent.com>
2024-10-31 13:43:54 +0200

    raising an error with a message

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return (True)
     except (TypeError, ValueError):
-        print("{}".format(sys.exc_info()[1], file = sys.strerr))
+        print("{}".format(sys.exc_info()[1]), file = sys.strerr)
         return (False)
