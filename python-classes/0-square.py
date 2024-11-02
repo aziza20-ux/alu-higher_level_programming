13344b2bb14eb02797d582a21795e171d42c5cc5
aziza20-ux <s.afadhali@alustudent.com>
2024-11-02 18:51:30 +0200

    documented class defimition

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
+"""this is a definition of a class, when you want to define a class you start with a "class" keyword followed by a class name, and then indent the data and members of the class."""
 class Square:
     pass
