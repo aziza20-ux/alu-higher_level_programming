83dbfa5065478de9929a54f00ef71feb90397245
aziza20-ux <s.afadhali@alustudent.com>
2024-11-03 18:33:30 +0200

    defining a class

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,7 @@
-"""this module is all about defining a class"""
+""" this module is all about defining a class"""
 
 
 #!/usr/bin/python3
 class Square:
-    """this is a definition of a class, when you want to define a class you start with a "class" keyword followed by a class name"""
+    """ this is a definition of a class, when you want to"""
     pass
