022603d38c7416b215f5123ada7f5a2fbd03fa64
aziza20-ux <s.afadhali@alustudent.com>
2024-11-04 10:22:55 +0200

    initializing a private class attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""""declaring a private instance attribute within a class contructor"
+class square:
+    """class contructor"""
+    def __init__(self, size):
+        self.__size = size
