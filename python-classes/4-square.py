126dc4366017dd08e0d5acf611d0f0e93148b12a
aziza20-ux <s.afadhali@alustudent.com>
2024-11-04 11:49:41 +0200

    square area

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -13,9 +13,9 @@ class Square:
     """Class constructor"""
     def __init__(self, size=0):
            if type(size) != int:
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
+               raise TypeError('size must be an integer')
+           if size < 0:
+               raise ValueError('size must be >= 0')
         self.__size = size
 
     """Size getter"""
@@ -23,7 +23,7 @@ class Square:
     def size(self):
         return self.__size
 
-    """size setter"""
+    """Size setter"""
         @size.setter
     def size(self, value):
         if type(value) != int:
