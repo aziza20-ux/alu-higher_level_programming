4f87d8055ebe89a5aebdb2c003992dbd19b342a1
aziza20-ux <s.afadhali@alustudent.com>
2024-11-04 10:56:40 +0200

    creating an object

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,7 +4,7 @@
     Instantiation with optional size
         size must be an integer
 """
-class square:
+class Square:
     """Class constructor"""
     def __init__(self, size=0):
         if type(size) != int:
