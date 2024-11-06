22ef8e68e51ef4d92dd52f23f271c2fed9982d66
aziza20-ux <s.afadhali@alustudent.com>
2024-11-06 22:17:49 +0200

    setters and getters

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ this module is all about defining a classes"""
+
+
 class Rectangle():
     """defining a class"""
     pass
---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" defining a classes with getters and setters"""
+class rectangle():
+    """ defining a class"""
+    def __init__(self, width=0, height=0):
+    self.__width = width
+    self.__height = height
+
+     """ getter for width"""
+    @property
+    def width(self):
+        return self.__width
+
+    """ setter for width"""
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+        """ getter for heigth"""
+     @property
+    def height(self):
+        return self.__height
+
+    """ setter for heigth"""
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
+
