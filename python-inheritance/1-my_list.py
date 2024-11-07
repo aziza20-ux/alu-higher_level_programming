7145ecd7fa3009df00ff7fe8de612cdfe2e807fd
aziza20-ux <s.afadhali@alustudent.com>
2024-11-07 22:36:19 +0200

    stressing

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" inhereting a class"""
+
+class my_list(list):
+    """MyList class - Inherits from list"""
+    def print_sorted(self):
+        """Prints a sorted list"""
+        print(sorted(self))
