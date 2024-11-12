c0edc88ad32960ec3a18e17238b8adb585e75f4c
aziza20-ux <s.afadhali@alustudent.com>
2024-11-12 12:51:47 +0200

    writing a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" reading from a file and create it if it doesn't exist, and count the number of characters"""
+""" reading from a file and create it if it doesn't exist, and count the number of characters."""
 
 
 def write_file(filename="", text=""):
-    """ a function to overwrite a file if it exists or create it if it doesn't exist"""
+    """ a function to overwrite, a file if it exists or create it if it doesn't exist."""
     with open(filename, mode='w' encoding="utf-8") as myfile:
         return myfile.write(text)
