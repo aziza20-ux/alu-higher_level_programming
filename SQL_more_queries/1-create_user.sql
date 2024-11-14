cb666cf5c00b73810c6ea10dbb66b0fca41dccc6
aziza20-ux <s.afadhali@alustudent.com>
2024-11-14 13:54:26 +0200

    granting privileges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,9 @@
 -- Creates the user user_0d_1 with all privileges.
 CREATE USER
-    IF NOT EXISTS user_0d_1@localhost
+    IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
     ON *.*
     TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT INSERT, SELECT ON user_2_db.* TO user_0d_2'@'localhost';
