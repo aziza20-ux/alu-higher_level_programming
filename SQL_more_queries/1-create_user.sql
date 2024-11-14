4fb00464f9082eba6b76233291c67bf3e76c1e7b
aziza20-ux <s.afadhali@alustudent.com>
2024-11-14 14:07:55 +0200

    hhhh

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,9 +1,11 @@
--- Creates the user user_0d_1 with all privileges.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-    ON *.*
-    TO 'user_0d_1'@'localhost';
+-- Create user_0d_1 as a root user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT INSERT, SELECT ON user_2_db.* TO user_0d_2'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+
+
+REVOKE ALL PRIVILEGES ON `user_2_db`.* FROM 'user_0d_2'@'localhost';
+GRANT SELECT, INSERT ON `user_2_db`.* TO 'user_0d_2'@'localhost';
