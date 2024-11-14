5e743603c38c824f5b79e3c407ca2f2edf294706
aziza20-ux <s.afadhali@alustudent.com>
2024-11-14 14:57:33 +0200

    name = California

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
--- comments
-SELECT id, name
+-- Script that lists all the cities of California registered in the database
+SELECT id, name -- Query to list all the cities from California
 FROM cities
-WHERE state_id = (
-	SELECT id,
-	FROM states
-	WHERE name = "California");
+WHERE state_id = ( -- Query to get the id of California
+      SELECT id
+      FROM states
+      WHERE name = "California");
