fbeaae423fcaafe14428120446b38f11afec6581
aziza20-ux <s.afadhali@alustudent.com>
2024-11-14 16:15:01 +0200

    priviliges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,9 +1,5 @@
--- Script that
-SELECT
-    GRANTEE,
-    PRIVILEGE_TYPE
-FROM
-    INFORMATION_SCHEMA.USER_PRIVILEGES
-WHERE
-    GRANTEE IN ("'user_0d_1'@'localhost'", "'user_0d_2'@'localhost'");
-
+-- Scrip
+-- Script that lists all privileges of the MySQL users
+-- Query to list all privileges (GRANT) of the MySQL users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
