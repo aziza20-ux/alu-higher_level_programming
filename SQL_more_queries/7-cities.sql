61abc53c6dc28d3df6053b1fafd0a8d7079fd36a
aziza20-ux <s.afadhali@alustudent.com>
2024-11-14 14:45:25 +0200

    forgn key

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,7 +1,9 @@
--- comments
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Script that creates a database and a table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa; -- Query to create database
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE, AUTO_INCREMENT, NOT NULL PRIMARY KEY
-state_id INT, NOT NULL, 
-FOREIGN KEY (state_id) REFERENCES states(id),
-name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities ( -- Query to create cities in hbtn_0d_usa
+       id INT UNIQUE AUTO_INCREMENT NOT NULL,
+       state_id INT NOT NULL,
+       name VARCHAR(256) NOT NULL,
+       PRIMARY KEY (id),
+       FOREIGN KEY (state_id) REFERENCES states(id));
