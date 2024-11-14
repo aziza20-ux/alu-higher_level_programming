9dc8e2f9892e0dd5bd72ffa4a6bcd0b322308ccb
aziza20-ux <s.afadhali@alustudent.com>
2024-11-14 17:36:59 +0200

    new commit

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,5 @@
--- id can't be null
-USE hbtn_0d_2;
-CREATE TABLE IF NOT EXISTS id_not_null ( id INT DEFAULT 1
-	name VARCHAR(256));
+-- Script that creates a table
+-- Query to create the table 'id_not_null' in MySQL server
+CREATE TABLE IF NOT EXISTS id_not_null (
+       id INT DEFAULT 1,
+       name VARCHAR(256));
