1922617b32f32312d45dd73e34c37dd5da881c14
aziza20-ux <s.afadhali@alustudent.com>
2024-11-20 16:23:54 +0200

    error

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """takes url & email, sends a POST request and displays the response"""
-from urllib import request, urlopen
+from urllib.request import Request, urlopen
 from urllib.error import HTTPError
 from sys import argv
 
-if __name__ ="__main__":
-
-    url = argv[1]
-    req = request(url)
+if __name__ == '__main__':
+    req = Request(argv[1])
     try:
-        with urlopen(req) as response
-        r = response.read()
-        print(r.decode('utf-8'))
+        with urlopen(req) as response:
+            r = response.read()
+            print(r.decode('utf-8'))
     except HTTPError as e:
-        print("code:" e.code)
+        print('Error code:', e.code)
