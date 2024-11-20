48f05d568d60ed363bb48cc492b37c37e454caf3
aziza20-ux <s.afadhali@alustudent.com>
2024-11-20 16:42:26 +0200

    add

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-""" Will  sing the song of ages and the angles cry holly"""
-from urllib import requests
+"""Takes URL, sends a request and displays the value of X-Request-Id"""
+import requests
 from sys import argv
 
-if __name__ == '__main__':
 
-req = request(argv[1])
-with urllib.urlopen(req) as response:
-    r = response.read()
-    print(r.getheader(X-Request-Id))
+if __name__ == '__main__':
+    r = requests.get(argv[1])
+    print(r.headers.get('x-Request-Id'))
