e2d9db95008f9be4d831b9a08453122d514e86de
aziza20-ux <s.afadhali@alustudent.com>
2024-11-20 17:55:43 +0200

    add

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""Yuejhsdhjfhcbvhcjvvhbfjc,hvjc hcfbgjfbc"""
+"""Takes URL sends request and displays value of X-Request-Id"""
 import urllib.request
 from sys import argv
 
 if len(argv) > 1:
     with urllib.request.urlopen(argv[1]) as response:
         print(response.getheader("X-Request-Id"))
-
-
