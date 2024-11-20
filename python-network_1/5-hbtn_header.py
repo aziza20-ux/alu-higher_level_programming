5b3aa7b58392c878f75d02a3bfb804614846a21b
aziza20-ux <s.afadhali@alustudent.com>
2024-11-20 16:38:55 +0200

    add

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Will  sing the song of ages and the angles cry holly"""
+from urllib import request
+from sys import argv
+
+if __name__ == '__main__':
+
+req = request(argv[1])
+with urllib.urlopen(req) as response:
+    r = response.read()
+    print(r.getheader(X-Request-Id))
