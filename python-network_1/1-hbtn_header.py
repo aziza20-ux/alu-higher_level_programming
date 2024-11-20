bc9bd5301aaa379a056e596e87bd16e9acf4bfab
aziza20-ux <s.afadhali@alustudent.com>
2024-11-20 15:45:38 +0200

    header

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,1 +1,10 @@
+#!usr/bin/python3
+"""Yuejhsdhjfhcbvhcjvvhbfjc,hvjc hcfbgjfbc"""
+import urllib.request
+from sys import argv
+
+if len(argv) > 1:
+    with urllib.request.urlopen(argv[1]) as response:
+        print(response.getheader("X-Request-Id"))
+
 
