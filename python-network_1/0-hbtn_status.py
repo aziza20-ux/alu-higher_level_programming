a0a9c91265bfa55a16daaa6a7c15f7b5d044dab1
aziza20-ux <s.afadhali@alustudent.com>
2024-11-20 15:22:28 +0200

    new commit

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,7 +4,8 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    url = "https://alx-intranet.hbtn.io/status"
+    request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
