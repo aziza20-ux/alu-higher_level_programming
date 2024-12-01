f6e8227c7728a4ff079f55bae4edb47bead63cf4
aziza20-ux <s.afadhali@alustudent.com>
2024-12-01 10:59:30 +0200

    cities

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-"""Thos project is about going to bedtime,
-and mapping."""
+'''
+script that lists all cities from the database
+'''
+
+import MySQLdb
 import sys
-import MYSQLdb
 
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306,
+        host='localhost')
 
-if __name__ = "__main__":
-    db = MYSQLdb.connect(
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            port=3306,
-            host='localhost'),
-    c = db.cursor()
-    c.execute('SELECT * FROM cities ORDER BY cities.id ASC;')
-    cities = c.fetchall()
-    for city in cities:
-        print(city)
+    cursor = db.cursor()
+    cursor.execute(
+        'SELECT cities.id, cities.name, states.name FROM cities JOIN states ON\
+        cities.state_id = states.id;')
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
