77d85485ca98d36772ac52838bc5fbfdcc412b61
aziza20-ux <s.afadhali@alustudent.com>
2024-12-01 17:01:48 +0200

    testing

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''
-Prints the first State,and the last date engine and using `sessionmaker(bind=engine)`.
+Prints the first State,and the last date engine 
+and using `sessionmaker(bind=engine)`.
 '''
 from sys import argv
 from sqlalchemy import create_engine
@@ -10,7 +11,8 @@ from model_state import State
 
 if __name__ == '__main__':
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], 
+            argv[2], argv[3]))
     InstanceSession = sessionmaker(bind=engine)
     session = InstanceSession()
 
