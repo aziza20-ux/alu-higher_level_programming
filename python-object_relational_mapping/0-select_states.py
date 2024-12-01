1312f4025872b24ab45dfdb4e819cf084fb0480a
aziza20-ux <s.afadhali@alustudent.com>
2024-12-01 09:34:36 +0200

    states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""This module is all about maping object,
-and connecting a database.
+"""
+Lists all states from the database hbtn_0e_0_usa
 """
 import sys
-import MYSQLdb
+import MySQLdb
 
-
-if __name__ == "__main__":
-db = MYSQLdb.connect( 
+if __name__ == '__main__':
+    db = MySQLdb.connect(
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
         port=3306,
         host='localhost')
-cursor=db.cursor()
-cursor.execute('SELECT * FROM states ORDER BY states.id ASC;')
-states = cursor.fetchall()
-for state in states:
-    print(state)
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM states ORDER BY states.id ASC;')
+
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
-"""this project is all about mapping,
-python objects with database.
-"""
+'''
+lists all states with a name starting with N
+'''
 
+import MySQLdb
 import sys
-import MYSQLdb
 
-if __name__ = "__main__":
-    db = MYSQLdb.connect(
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            port=3306
-            host='localhost')
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306,
+        host='localhost')
+
     cursor = db.cursor()
-    cursor.execute('SELECT * FROM states WHERE LIKE N% ORDER BY states.id ASC;')
+    cursor.execute("SELECT * \
+                    FROM states \
+                    WHERE CONVERT(`name` USING Latin1) \
+                    COLLATE Latin1_General_CS \
+                    LIKE 'N%';")
+
     states = cursor.fetchall()
     for state in states:
         print(state)
-        cursor.close()
-        db.close()
+    cursor.close()
+    db.close()
