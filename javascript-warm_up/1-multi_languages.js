87e9877736d7cc49a8dc9bff92d50c4d21bb9884
aziza20-ux <s.afadhali@alustudent.com>
2025-01-22 15:50:34 +0200

    this is the real commit

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const a = 'C is fun'; 
-const b = 'python is cool'; 
-const c = 'javascript is amazing';
+const b = 'Python is cool'; 
+const c = 'Javascript is amazing';
 console.log(a);
 console.log(b);
 console.log(c);
---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const argv = process.argv.slice(2)
+if (argv == 0){
+	console.log('No argument');
+}
+else if (argv == 1){
+	console.log("argument found");
+}
+else {
+	console.log("argument found");
+}
