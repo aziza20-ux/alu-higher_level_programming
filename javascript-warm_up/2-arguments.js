d448f5616e0ceb7bfc7494a53dd1bf06b02aadd2
aziza20-ux <s.afadhali@alustudent.com>
2025-01-22 16:08:05 +0200

    this is the real commit

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -5,8 +5,8 @@ if (argv == 0){
 	console.log('No argument');
 }
 else if (argv == 1){
-	console.log("argument found");
+	console.log("Argument found");
 }
 else {
-	console.log("argument found");
+	console.log("Arguments found");
 }
