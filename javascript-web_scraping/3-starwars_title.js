aef22cd51662637ff7eb46c935338f2674bd52f7
aziza20-ux <s.afadhali@alustudent.com>
2025-02-07 09:17:39 +0200

    the title movie

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const episodnber = process.argv[2];
-const url_api = https:'//swapi-api.alx-tools.com/api/films/:id';
+const url_api = https:'//swapi-api.alx-tools.com/api/films/';
 
 request(url_api + episodnber, function(err, response, body){
 if (err) {
