b22cae6ab03dcd4eca7b5f8226d9f995aef99402
aziza20-ux <s.afadhali@alustudent.com>
2025-02-12 17:40:01 +0200

    this is the first commit

---
+++ javascript-web_jquery/4-script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+$('#toggle_header').click(function () {
+if ($('header').hasClass('.red')) {
+	$('header').removeClass('.red');
+	$('header').addClass('.green');
+	else {
+		 $('header').removeClass('.green');
+		$('header').addClass('.red');
+	};
+};
+});
+});
